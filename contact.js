7e4ef2d88c266f10cf8f65e809225c80bb1722c8
atsushi-wakamatsu <1106Wkm@gmail.com>
2021-02-11 03:22:33 +0900

    fix contact.js

--- contact.js
+++ contact.js
@@ -1,16 +1,22 @@
-$.ajax({
-    success : function(response){
-        alert('成功');
-    },
-    error: function(){
-        //通信失敗時の処理
-        alert('通信失敗');
-    }
-});
+// ajaxが動くか確認
+// $.ajax({
+//     success : function(response){
+//         alert('通信成功');
+//     },
+//     error: function(){
+//         //通信失敗時の処理
+//         alert('通信失敗');
+//     }
+// });
 
 window.contact = window.contact || {};
 
-$("#submit").on('click', function(){
+$("#submit").on('click', function(e){
+    e.preventDefault(); // フォームから送信されたデータ（通常のform_withのsubmitによるもの）とAjax関数によって送信されたデータを二重に送らないようにする
+    window.contact.send(this);
+});
+
+window.contact.send = function(){
     if($('input[id="name"]').val() && $('input[id="email"]').val() && $('textarea[id="message"]').val()){
         var name = $('input[id="name"]').val() 
         var email = $('input[id="email"]').val() 
@@ -27,10 +33,9 @@ $("#submit").on('click', function(){
             window.contact.ajax(data);
         });
     }
-});
+}
 
 window.contact.ajax = function(data){
-    alert('ajax');
     var url = 'https://script.google.com/macros/s/AKfycbzDgkDCNdVj-qGl4TEMuGaVKU6CD4RPheAU1iqaFUDu41QtNo0yPgbMiw/exec'; // Change here: Your GAS URL here
     $.ajax({
         url: url,
@@ -45,35 +50,13 @@ window.contact.ajax = function(data){
             alert('送信失敗'); 
             return;
         }
-        console.log('送信完了');
         alert('送信完了');
     }).fail(function(XMLHttpRequest, textStatus, errorThrown){
-        alert(XMLHttpRequest.status);
-        alert(textStatus);
-        alert(errorThrown);
-        console.log('送信失敗');
+        console.log(XMLHttpRequest.status);
+        console.log(textStatus);
+        console.log(errorThrown);
         alert('送信失敗'); 
     }).always(function(){
         location.href="index.html";
     })
 }
-
-
-jQuery(function($){
-    $(document).ajaxSend(function() {
-        $("#overlay").fadeIn(300);　
-    });
-    
-    $('#button').click(function(){
-        $.ajax({
-            type: 'GET',
-            success: function(data){
-                console.log(data);
-            }
-        }).done(function() {
-            setTimeout(function(){
-                $("#overlay").fadeOut(300);
-            },500);
-        });
-    }); 
-});
